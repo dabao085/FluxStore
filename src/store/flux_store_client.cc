1576aed0c57e3a70e1c999d399259f8a6ad10166
chengxiang <chengxiang085@gmail.com>
2025-01-09 15:55:11 +0800

    feat(FluxStore): Replace standard output with glog.

--- src/store/flux_store_client.cc
+++ src/store/flux_store_client.cc
@@ -1,28 +1,28 @@
 #include "store/flux_store_client.h"
+#include "glog/logging.h"
 
 void FluxStoreCLI::upload(const std::string& file_path) {
-  std::cout << "Uploading file: " << file_path << std::endl;
+  LOG(INFO) << "Uploading file: " << file_path;
 
   std::string data;
   leveldb::Status status;
   size_t file_size = 0;
   status = local_fs_->ReadFile(file_path, &data, &file_size);
   if (!status.ok()) {
-    std::cerr << "Fail to read from local fs, file=" << file_path << std::endl;
+    LOG(ERROR) << "Fail to read from local fs, file=" << file_path;
     return;
   }
 
   status = flux_fs_->WriteFile(file_path, data);
   if (!status.ok()) {
-    std::cerr << "Fail to write to flux fs, file=" << file_path << std::endl;
+    LOG(ERROR) << "Fail to write to flux fs, file=" << file_path;
     return;
   }
 }
 
 void FluxStoreCLI::download(const std::string& file_path,
                             const std::string& destination) {
-  std::cout << "Downloading file: " << file_path << " to " << destination
-            << std::endl;
+  LOG(INFO) << "Downloading file: " << file_path << " to " << destination;
 
   size_t file_size = flux_fs_->GetFileSize(file_path);
   if (file_size != 0) {
@@ -30,33 +30,33 @@ void FluxStoreCLI::download(const std::string& file_path,
     leveldb::Status status;
     status = flux_fs_->ReadFile(file_path, &data, &file_size);
     if (!status.ok()) {
-      std::cerr << "Failed to read file " << file_path
-                << ", error=" << status.ToString() << std::endl;
+      LOG(ERROR) << "Failed to read file " << file_path
+                 << ", error=" << status.ToString();
       return;
     } else {
       status = local_fs_->WriteFile(destination, data);
       if (!status.ok()) {
-        std::cerr << "Failed to write file " << destination
-                  << ", error=" << status.ToString() << std::endl;
+        LOG(ERROR) << "Failed to write file " << destination
+                   << ", error=" << status.ToString();
       }
     }
   } else {
-    std::cout << "size of file " << file_path << " is 0" << std::endl;
+    LOG(WARNING) << "size of file " << file_path << " is 0";
   }
 }
 
 void FluxStoreCLI::remove(const std::string& file_path) {
-  std::cout << "Deleting file: " << file_path << std::endl;
+  LOG(INFO) << "Deleting file: " << file_path;
   // 调用具体的删除逻辑
   leveldb::Status status = flux_fs_->DeleteFile(file_path);
   if (!status.ok()) {
-    std::cerr << "Failed to remove file " << file_path << std::endl;
+    LOG(ERROR) << "Failed to remove file " << file_path;
   }
 }
 
 void FluxStoreCLI::run(int argc, char* argv[]) {
   if (argc < 2) {
-    std::cout << "Usage: <command> <file_path> [destination]" << std::endl;
+    LOG(ERROR) << "Usage: <command> <file_path> [destination]";
     return;
   }
 
@@ -68,6 +68,6 @@ void FluxStoreCLI::run(int argc, char* argv[]) {
   } else if (command == "delete" && argc == 3) {
     remove(argv[2]);
   } else {
-    std::cout << "Invalid command or arguments." << std::endl;
+    LOG(ERROR) << "Invalid command or arguments.";
   }
 }
